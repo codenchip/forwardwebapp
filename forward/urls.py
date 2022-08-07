d4033c0aad5cf533c6a4c7cfb2be3efb4b70440a
Daren Tan <darentan.work@gmail.com>
2022-08-07 11:03:06 +0800

    completed book app

--- forward/urls.py
+++ forward/urls.py
@@ -14,14 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include, re_path
+from django.conf.urls import include
+from django.urls import path,re_path
 from hello_world import views
+from forward.settings import DEBUG, STATIC_URL, STATIC_DIR, MEDIA_URL,MEDIA_ROOT
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     re_path(r'^$',views.index,name='index'),
     re_path(r'^special/',views.special,name='special'),
     re_path(r'^hello_world/',include('hello_world.urls')),
+    re_path(r'^book/',include('book.urls')),
     re_path(r'^logout/$', views.user_logout, name='logout'),
 ]
+
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root = MEDIA_ROOT)
+
